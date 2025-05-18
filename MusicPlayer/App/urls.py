2b1f58c73c6338d1e50999ac6848078f0258aa9b
Atharv Desai <110416773+Atharv064@users.noreply.github.com>
2025-05-18 21:06:15 +0530

    Delete MusicPlayer directory

--- MusicPlayer/App/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from . import views
-from django.urls import path
-
-
-urlpatterns=[
-    path('',views.index,name="index"),
-]
